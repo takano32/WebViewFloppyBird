81fa4ee093f770ed357277112c24bca811551510
TAKANO Mitsuhiro <takano32@gmail.com>
2017-11-05 10:09:17 +0900

    Initial Commit

---
+++ WebViewFloppyBird/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WebViewFloppyBird
+//
+//  Created by 高野光弘 on 11/5/29 H.
+//  Copyright © 29 TAKANO Mitsuhiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
